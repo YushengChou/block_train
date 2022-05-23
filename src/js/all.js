c570e5a1600231077c62dc5e30bdf3364e15d419
mm7217373 <mm7217373@gmail.com>
2022-05-23 17:12:04 +0800

    index.html 增加主要結構 head.html 引入JQ CDN style.scss import index.scss var.scss 加入高度 add index.scss all.js JS沒反應

---
+++ src/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('1')
+$(function(){
+    console.log('123')
+    $('.index').on('wheel', function(e){
+        e.preventDefault();
+        $('.index').scrollLeft() += e.deltaY;
+    })
+    console.log('1223')
+
+});
