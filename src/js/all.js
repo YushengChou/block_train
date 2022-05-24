ea5517895d096122ca232d7f0c4942bd2350b463
mm7217373 <mm7217373@gmail.com>
2022-05-24 15:36:02 +0800

    all.js 先用JS寫水平移動視窗

--- src/js/all.js
+++ src/js/all.js
@@ -1,10 +1,14 @@
 "use strict";
-console.log('1')
-$(function(){
-    // console.log('123')
-    // $('.index').on('wheel', function(e){
-    //     e.preventDefault();
-    //     // $('.index').scrollLeft() += e.deltaY;
-    // })
-    console.log('2')
-});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let container = document.querySelector('.index');
+    // 增加滑鼠滾輪事件
+    container.addEventListener('wheel', function (e) {
+        // 先停止滾輪預設行為
+        e.preventDefault;
+        // 利用 scrollLeft 屬性使容器向左移動，滾動距離差值(deltaY)
+        container.scrollLeft += e.deltaY;
+    })
+
+})
