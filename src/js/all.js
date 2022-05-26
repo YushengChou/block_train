fd19235d8cadc90e302961fcc0c7134d5f985220
mm7217373 <mm7217373@gmail.com>
2022-05-26 23:05:30 +0800

    header 增加當前時間

--- src/js/all.js
+++ src/js/all.js
@@ -2,6 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    // 垂直改水平
     let container = document.querySelector('.index');
     // 增加滑鼠滾輪事件
     container.addEventListener('wheel', function (e) {
@@ -10,5 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
         // 利用 scrollLeft 屬性使容器向左移動，滾動距離差值(deltaY)
         container.scrollLeft += e.deltaY;
     })
+
+    // 抓取當前時分秒
+    function currentTime(){
+        const date = new Date();
+        document.getElementById('time').innerHTML = date.toLocaleTimeString();
+    }
+    setInterval(currentTime, 1000);
 
 })
