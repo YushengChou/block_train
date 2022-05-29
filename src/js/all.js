e22e839375fe3639ebdbc7518713cc379bde2861
mm7217373 <mm7217373@gmail.com>
2022-05-29 18:36:05 +0800

    add clock for navbar

--- src/js/all.js
+++ src/js/all.js
@@ -19,6 +19,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     setInterval(currentTime, 1000);
 
+    // 時鐘
+    let dateInfo = new Date();
+    let hr = dateInfo.getHours() > 12 ? dateInfo.getHours() - 12 : dateInfo.getHours();
+    let min = dateInfo.getMinutes();
+    let sec = dateInfo.getSeconds();
+    // 毫秒數
+    let milsec = dateInfo.getMilliseconds();
+    // 時分秒角度
+    let hrAngle = hr * 30 + (min * 6 / 12);
+    let minAngle = min * 6 + (sec * 6 / 60);
+    let secAngle = sec * 6 + (milsec * 0.36 / 1000);
+
+    function setAngle(wrapper, angle) {
+        document.querySelector('.' + wrapper).style.transform = 'translate(-50%, -50%) rotate(' + angle + 'deg)';
+    }
+
+    setAngle('hr', hrAngle);
+    setAngle('min', minAngle);
+    setAngle('sec', secAngle);
+
     // 取得天氣(串接氣象局API，Authorization後為授權碼)
     fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-CFC71FBE-3A1B-4185-B916-CD729183112A&format=JSON')
         .then(res => {
